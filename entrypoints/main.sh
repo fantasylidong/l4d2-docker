025840c63f6e7917a064c75d1bbf59853b6a1e8a
zombie110year <zombie110year@outlook.com>
2024-03-09 11:12:01 +0800

    将entrypoint运行主体包装在 main 函数里并放到文件最前面

--- entrypoints/main.sh
+++ entrypoints/main.sh
@@ -1,102 +1,11 @@
 #!/bin/bash
+main() {
 #Softlink l4d2 maps to addons folder
 #It would more convenience while you want add custom map. Exspecially when you have sourcemod plugins
 #you just need mount your extra map folder to docker container /map . 
 ln  -s  /map/*  l4d2/left4dead2/addons/
 ln  -s  /map2/*  l4d2/left4dead2/addons/
-oldpluginpackage(){
-	echo "\n\"$steamid\" \"99:z\"" >> /home/louis/l4d2/left4dead2/addons/sourcemod/configs/admins_simple.ini
-	echo "sv_steamgroup \"$steamgroup\"" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
-	echo "rcon_password \"$password\"" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
-}
-newpluginpackage(){
-	sed -i "s/13333337/$steamgroup/g"  /home/louis/l4d2/left4dead2/cfg/server.cfg
-	sed -i "s/CompetitiveRework/annehappy/g" /home/louis/l4d2/left4dead2/cfg/server.cfg
-	sed -i "s/WowYouKnowThePasswordHere/$password/g" /home/louis/l4d2/left4dead2/cfg/server.cfg
-}
-cloudconfig(){
-	#cloud server settings
-	#插件处理hidden
-	#echo "\nsv_tags hidden" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
-	#sed -i "s/nb_update_frequency\ 0.014/nb_update_frequency\ 0.024/" /home/louis/l4d2/left4dead2/cfg/server.cfg
-	#sed -i "s/fps_max\ 150/fps_max\ 0/" /home/louis/l4d2/left4dead2/cfg/server.cfg
-	if [! -n "$serverid" ]
-	then
-		sed -i "47 s/\"2\"/\"16\"/" /home/louis/l4d2/left4dead2/addons/sourcemod/configs/sourcebans/sourcebans.cfg
-	fi
-	#cp /home/louis/l4d2/left4dead2/addons/hostname.txt /home/louis/l4d2/left4dead2/addons/sourcemod/configs/hostname/
-	cp /home/louis/l4d2/left4dead2/addons/advertisements* /home/louis/l4d2/left4dead2/addons/sourcemod/configs/
-	cp /home/louis/l4d2/left4dead2/addons/admins_simple.ini /home/louis/l4d2/left4dead2/addons/sourcemod/configs/
- 	cp /home/louis/l4d2/left4dead2/addons/shared_plugins.cfg /home/louis/l4d2/left4dead2/cfg/cfgogl/annehappy/shared_plugins.cfg
-}
 
-localconfig(){
-	rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/optional/specrates.smx
- 	if [ "$challenge" != "true" ];
-  	then
-		sed -i "s/fps_max\ 0/fps_max\ 150/" /home/louis/l4d2/left4dead2/cfg/server.cfg
-  	else
-   		sed -i "18 s/0/1/" /home/louis/l4d2/left4dead2/cfg/addonconfig.cfg
-   		sed -i "s/nb_update_frequency\ 0.024/nb_update_frequency\ 0.014/" /home/louis/l4d2/left4dead2/cfg/server.cfg
-     		echo "\nsm plugins load anticheat/l4d2_nobhaps.smx" >> /home/louis/l4d2/left4dead2/cfg/generalfixes.cfg
-  	fi
-	if [ "$PORT" = "2330" ];
-	then
-		#git -C /home/louis/CompetitiveWithAnne/ checkout test
-		cp  -r /home/louis/CompetitiveWithAnne/* l4d2/left4dead2/
-		rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/optional/AnneHappy/sam_vs.smx
-		sed -i "256 s/-secure/-insecure/"  /home/louis/entrypoint.sh
-		sed -i "s/join_autoupdate\ 1/join_autoupdate\ 0/g" /home/louis/l4d2/left4dead2/cfg/cfgogl/*/shared_settings.cfg
-		sed -i "s/join_autoupdate\ 4/join_autoupdate\ 0/g" /home/louis/l4d2/left4dead2/cfg/cfgogl/*/shared_settings.cfg
-		echo "sm_cvar join_autoupdate 0" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
-	fi
-	if [ "$PORT" = "2340" ];
-	then
-		sed -i 's/AnneHappy6.cfg/AnneHappy4.cfg/g' /home/louis/l4d2/left4dead2/cfg/cfgogl/annehappy/confogl_plugins.cfg
-		sed -i 's/ai_Tank_StopDistance\ 135/ai_Tank_StopDistance\ 145/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
-		sed -i 's/ai_TankAirAngleRestrict\ 57/ai_TankAirAngleRestrict\ 60/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
-	fi
-	if [ "$PORT" = "2341" ];
-	then
-		sed -i 's/AnneHappy6.cfg/AnneHappy5.cfg/g' /home/louis/l4d2/left4dead2/cfg/cfgogl/annehappy/confogl_plugins.cfg
-		sed -i 's/ai_Tank_StopDistance\ 135/ai_Tank_StopDistance\ 140/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
-		sed -i 's/ai_TankAirAngleRestrict\ 57/ai_TankAirAngleRestrict\ 60/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
-	fi
-	if [ "$PORT" = "2342" ];
-	then
-		sed -i 's/ai_Tank_StopDistance\ 135/ai_Tank_StopDistance\ 135/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
-		sed -i 's/ai_TankAirAngleRestrict\ 57/ai_TankAirAngleRestrict\ 60/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
-	fi
-}
-copydanceresource(){
-	cp -r /home/louis/anne/left4dead2/sound/ l4d2/left4dead2/
-	cp -r /home/louis/anne/left4dead2/models/ l4d2/left4dead2/
-}
-anneremovemysql(){
-	#修改hostname插件端口对应port环境变量
-	if [ -n "$PORT" ]
-	then
-		sed -i "s/2330/$PORT/g" /home/louis/l4d2/left4dead2/addons/sourcemod/configs/hostname/hostname.txt
-	fi
-	
-	if [ -n "$hostname" ]
-	then
-		sed -i "s/Anne电信测试服/$hostname/g" /home/louis/l4d2/left4dead2/addons/sourcemod/configs/hostname/hostname.txt
-	fi
-	
-	if [ -n "$mysqlexist" ]
-	then
-		rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/l4d_stats.smx
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/chat-processor.smx
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/hextags.smx
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/lilac.smx
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/sbpp_*
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/rpg.smx
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/chatlog.smx
-                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/veterans.smx
-                cp /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/disabled/rpg.smx /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/
-	fi
-}
 # plugins Config
 if [ ! -d "/home/louis/l4d2/left4dead2/addons/sourcemod/" ];
 then
@@ -290,3 +199,98 @@ fi
 
 # Start Game
 cd l4d2 && ./srcds_run -console -game left4dead2 -ip 0.0.0.0 -tickrate 100 -port "$PORT"  +maxplayers "$PLAYERS" +map "$MAP" -secure
+}
+oldpluginpackage(){
+	echo "\n\"$steamid\" \"99:z\"" >> /home/louis/l4d2/left4dead2/addons/sourcemod/configs/admins_simple.ini
+	echo "sv_steamgroup \"$steamgroup\"" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
+	echo "rcon_password \"$password\"" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
+}
+newpluginpackage(){
+	sed -i "s/13333337/$steamgroup/g"  /home/louis/l4d2/left4dead2/cfg/server.cfg
+	sed -i "s/CompetitiveRework/annehappy/g" /home/louis/l4d2/left4dead2/cfg/server.cfg
+	sed -i "s/WowYouKnowThePasswordHere/$password/g" /home/louis/l4d2/left4dead2/cfg/server.cfg
+}
+cloudconfig(){
+	#cloud server settings
+	#插件处理hidden
+	#echo "\nsv_tags hidden" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
+	#sed -i "s/nb_update_frequency\ 0.014/nb_update_frequency\ 0.024/" /home/louis/l4d2/left4dead2/cfg/server.cfg
+	#sed -i "s/fps_max\ 150/fps_max\ 0/" /home/louis/l4d2/left4dead2/cfg/server.cfg
+	if [! -n "$serverid" ]
+	then
+		sed -i "47 s/\"2\"/\"16\"/" /home/louis/l4d2/left4dead2/addons/sourcemod/configs/sourcebans/sourcebans.cfg
+	fi
+	#cp /home/louis/l4d2/left4dead2/addons/hostname.txt /home/louis/l4d2/left4dead2/addons/sourcemod/configs/hostname/
+	cp /home/louis/l4d2/left4dead2/addons/advertisements* /home/louis/l4d2/left4dead2/addons/sourcemod/configs/
+	cp /home/louis/l4d2/left4dead2/addons/admins_simple.ini /home/louis/l4d2/left4dead2/addons/sourcemod/configs/
+ 	cp /home/louis/l4d2/left4dead2/addons/shared_plugins.cfg /home/louis/l4d2/left4dead2/cfg/cfgogl/annehappy/shared_plugins.cfg
+}
+
+localconfig(){
+	rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/optional/specrates.smx
+ 	if [ "$challenge" != "true" ];
+  	then
+		sed -i "s/fps_max\ 0/fps_max\ 150/" /home/louis/l4d2/left4dead2/cfg/server.cfg
+  	else
+   		sed -i "18 s/0/1/" /home/louis/l4d2/left4dead2/cfg/addonconfig.cfg
+   		sed -i "s/nb_update_frequency\ 0.024/nb_update_frequency\ 0.014/" /home/louis/l4d2/left4dead2/cfg/server.cfg
+     		echo "\nsm plugins load anticheat/l4d2_nobhaps.smx" >> /home/louis/l4d2/left4dead2/cfg/generalfixes.cfg
+  	fi
+	if [ "$PORT" = "2330" ];
+	then
+		#git -C /home/louis/CompetitiveWithAnne/ checkout test
+		cp  -r /home/louis/CompetitiveWithAnne/* l4d2/left4dead2/
+		rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/optional/AnneHappy/sam_vs.smx
+		sed -i "256 s/-secure/-insecure/"  /home/louis/entrypoint.sh
+		sed -i "s/join_autoupdate\ 1/join_autoupdate\ 0/g" /home/louis/l4d2/left4dead2/cfg/cfgogl/*/shared_settings.cfg
+		sed -i "s/join_autoupdate\ 4/join_autoupdate\ 0/g" /home/louis/l4d2/left4dead2/cfg/cfgogl/*/shared_settings.cfg
+		echo "sm_cvar join_autoupdate 0" >> /home/louis/l4d2/left4dead2/cfg/server.cfg
+	fi
+	if [ "$PORT" = "2340" ];
+	then
+		sed -i 's/AnneHappy6.cfg/AnneHappy4.cfg/g' /home/louis/l4d2/left4dead2/cfg/cfgogl/annehappy/confogl_plugins.cfg
+		sed -i 's/ai_Tank_StopDistance\ 135/ai_Tank_StopDistance\ 145/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
+		sed -i 's/ai_TankAirAngleRestrict\ 57/ai_TankAirAngleRestrict\ 60/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
+	fi
+	if [ "$PORT" = "2341" ];
+	then
+		sed -i 's/AnneHappy6.cfg/AnneHappy5.cfg/g' /home/louis/l4d2/left4dead2/cfg/cfgogl/annehappy/confogl_plugins.cfg
+		sed -i 's/ai_Tank_StopDistance\ 135/ai_Tank_StopDistance\ 140/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
+		sed -i 's/ai_TankAirAngleRestrict\ 57/ai_TankAirAngleRestrict\ 60/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
+	fi
+	if [ "$PORT" = "2342" ];
+	then
+		sed -i 's/ai_Tank_StopDistance\ 135/ai_Tank_StopDistance\ 135/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
+		sed -i 's/ai_TankAirAngleRestrict\ 57/ai_TankAirAngleRestrict\ 60/' /home/louis/l4d2/left4dead2/cfg/vote/hard_off.cfg
+	fi
+}
+copydanceresource(){
+	cp -r /home/louis/anne/left4dead2/sound/ l4d2/left4dead2/
+	cp -r /home/louis/anne/left4dead2/models/ l4d2/left4dead2/
+}
+anneremovemysql(){
+	#修改hostname插件端口对应port环境变量
+	if [ -n "$PORT" ]
+	then
+		sed -i "s/2330/$PORT/g" /home/louis/l4d2/left4dead2/addons/sourcemod/configs/hostname/hostname.txt
+	fi
+	
+	if [ -n "$hostname" ]
+	then
+		sed -i "s/Anne电信测试服/$hostname/g" /home/louis/l4d2/left4dead2/addons/sourcemod/configs/hostname/hostname.txt
+	fi
+	
+	if [ -n "$mysqlexist" ]
+	then
+		rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/l4d_stats.smx
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/chat-processor.smx
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/hextags.smx
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/lilac.smx
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/sbpp_*
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/rpg.smx
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/chatlog.smx
+                rm /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/veterans.smx
+                cp /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/disabled/rpg.smx /home/louis/l4d2/left4dead2/addons/sourcemod/plugins/extend/
+	fi
+}
+main
