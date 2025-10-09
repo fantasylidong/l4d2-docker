dd4b20282629dea7d5ec33eb3429d2ada1a56b59
morzlee <fantasylidong@gmail.com>
2025-10-09 11:04:59 +0800

    Update run.sh

--- entrypoints/run.sh
+++ entrypoints/run.sh
@@ -1,5 +1,5 @@
 if [ -n "$IP" ]; then
-  cd l4d2 && ./srcds_run -console -game left4dead2 -ip "$IP" -tickrate 128 -port "$PORT" +maxplayers "$PLAYERS" +map "$MAP" +sv_setmax 31 -secure
+  cd l4d2 && ./srcds_run -console -game left4dead2 -ip "$IP" -tickrate 128 -port "$PORT" +maxplayers 31 +map "$MAP" -secure
 else
-  cd l4d2 && ./srcds_run -console -game left4dead2 -ip 0.0.0.0 -tickrate 128 -port "$PORT" +maxplayers "$PLAYERS" +map "$MAP" +sv_setmax 31 -secure
+  cd l4d2 && ./srcds_run -console -game left4dead2 -ip 0.0.0.0 -tickrate 128 -port "$PORT" +maxplayers 31 +map "$MAP" -secure
 fi
